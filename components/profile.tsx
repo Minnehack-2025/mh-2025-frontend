cec74b28f9e5192a500bfe4799e77eafa9acc095
Aostii <lieuost@gmail.com>
2025-02-09 03:42:02 -0600

    added profile.tsx as a placeholder if things fall apart

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/profile.css';
+import Image from 'next/image';
 
 interface ProfileProps {
     name: string;
@@ -11,7 +12,7 @@ interface ProfileProps {
     const Profile: React.FC<ProfileProps> = ({ name, bio, avatarUrl, backgroundUrl, socialLinks }) => {
     return (
         <div className="profile" style={{ backgroundImage: `url(${backgroundUrl})` }}>
-            <img src={avatarUrl} alt={`${name}'s avatar`} className="profile-avatar" />
+            <Image src={avatarUrl} alt={`${name}'s avatar`} className="profile-avatar" />
             <h2 className="profile-name">{name}</h2>
             <p className="profile-bio">{bio}</p>
             {socialLinks && (
@@ -25,6 +26,6 @@ interface ProfileProps {
             )}
         </div>
     );
-
+    }
 export default Profile;              
 
