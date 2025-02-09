d5229ead1bac42fa48522cd3febd1242c8927e84
Aostii <lieuost@gmail.com>
2025-02-08 22:46:44 -0600

    hello

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import Image from 'next/image';
-import Profile from '@/images/ss.png';
-import styles from './topbar.module.css';
 
 const TopBar: React.FC = () => {
   return (
-    <div className={`${styles.topBar} flex flex-col gap-2 w-full mx-auto`}>
+    <div className="flex flex-col gap-2 w-full mx-auto">
       <div className='bg-gray-300 flex flex-row justify-between items-center p-4'>
-        <h1 className='text-xl font-bold'>TopBar</h1>
+        <h1 className='text-xl font-bold'>Topbar :D</h1>
         <div className='flex flex-row items-center gap-2'>
-          <div className='w-8 h-8'>
-            <Image src={Profile} alt="TopBar" />
-          </div>
           <nav>
             <ul className='flex flex-row gap-4'>
-              <li><a href="#home" className='text-gray-700 hover:text-gray-900'>Home</a></li>
-              <li><a href="#about" className='text-gray-700 hover:text-gray-900'>About</a></li>
-              <li><a href="#contact" className='text-gray-700 hover:text-gray-900'>Contact</a></li>
+              <li><a href="#subsection1" className='text-black hover:text-black-800'>Subsection 1</a></li>
+              <li><a href="#subsection2" className='text-black hover:text-black-800'>Subsection 2</a></li>
+              <li><a href="#subsection3" className='text-black hover:text-black-800'>Subsection 3</a></li>
             </ul>
           </nav>
         </div>
