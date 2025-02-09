36e50435137f74299d62c36a66e25b81547b1703
Jiaxi Tang <jt.jiaxitang@gmail.com>
2025-02-08 23:36:40 -0600

    chnages n gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,52 @@
 'use client'
 
 import TinderCardArray from "@/components/TinderCardArray";
+import { Button } from "@/components/ui/button"
 import { useState } from "react";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { AiFillCheckCircle } from "react-icons/ai";
 
 export default function Home() {
   const [eventNum, setEventNumb] = useState(0);
+  const [isHidden, setIsHidden] = useState(true);
 
   const handleNo = () => {
     setEventNumb(eventNum + 1);
   }
 
   const handleYes = () => {
-    
+    setIsHidden(false);
+  }
+
+  const handleCongratsClose = () => {
+    setIsHidden(true);
+    setEventNumb(eventNum + 1);
   }
 
   return (
-    <div>
-      <TinderCardArray num={eventNum}/>
-      <div className="flex flex-row justify-between w-[60vw] mx-auto mt-4"> 
-        <button onClick={handleNo}>
-          <AiFillCloseCircle size={80}/>
-        </button>
-        <button onClick={handleYes}>
-          <AiFillCheckCircle size={80}/>
-        </button>
-      </div>
-    </div>
+    <>
+      {isHidden ? (
+        <>
+          <div className="w-[70vw] h-[90vw] max-h-[75vh]">
+            <TinderCardArray index={eventNum}/>
+          </div>
+          <div className="flex flex-row justify-between w-[60vw] mx-auto mt-4"> 
+            <button onClick={handleNo}>
+              <AiFillCloseCircle size={80}/>
+            </button>
+            <button onClick={handleYes}>
+              <AiFillCheckCircle size={80}/>
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className={`w-full h-full flex flex-col items-center bg-white ${isHidden ? 'hidden' : ''}`}>
+          <p className="text-center">You are registered!</p>
+          <Button onClick={handleCongratsClose}>
+            Next
+          </Button>
+        </div>
+      )}
+    </>
   );
 }
--- components/TinderCardArray.tsx
+++ components/TinderCardArray.tsx
@@ -4,20 +4,32 @@ import TinderCard from "@/components/TinderCard";
 
 interface TinderCardArrayProps {
   eventsArray?: string[];
-  num: number;
+  index: number;
 }
 
-const TinderCardArray: React.FC<TinderCardArrayProps> = ({num}) => {
+const TinderCardArray = ({index} : TinderCardArrayProps) => {
+// async function TinderCardArray({num} : TinderCardArrayProps) {
   const eventsArray = ['history', 'hi']
+  // const response = await fetch("https://api.connectionhub.me/events", {
+  //   cache: "no-store",
+  // });
+  // const data = await response.json();
 
   return (
     <div>
-      {eventsArray
-        .filter((_, index) => index === num)
-        .map((event, index) => (
-          <TinderCard key={index} name={event} />
-      ))}
-  </div>
+      { index > eventsArray.length ? (    
+        eventsArray
+          .filter((_, index) => index === index)
+          .map((event, index) => (
+            <TinderCard key={index} name={event} />
+        ))
+        ) : (
+          <p className='text-center mx-auto'>
+            You reached the end!
+          </p>
+        )
+      }
+   </div>
   )
 }
 
